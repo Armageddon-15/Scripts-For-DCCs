9189a0e01a57f007e1bee0fedb23dbbfcba2f96a
Armageddon-15 <hjc014@126.com>
2023-07-30 22:42:41 +0800

    rename mayamainwindow

--- Maya/plug-in/armageddon/GUI/MayaMainWindow.py
+++ Maya/plug-in/armageddon/GUI/MayaMainWindow.py
@@ -14,7 +14,8 @@ def setWidgetAsMayaMainWindow(widget, title_name="DefaultTitle", object_name=Non
     except Exception as e:
         print(e)
         
-    widget.setObjectName(object_name)
+    if object_name is not None:
+        widget.setObjectName(object_name)
     
     MAYA_MAIN_WINDOW_PTR = omui.MQtUtil.mainWindow() 
     MAYA_MAIN_WINDOW = wrapInstance(long(MAYA_MAIN_WINDOW_PTR), QWidget) 
@@ -22,3 +23,12 @@ def setWidgetAsMayaMainWindow(widget, title_name="DefaultTitle", object_name=Non
     widget.setWindowFlags(Qt.Window)
     
     widget.setWindowTitle(title_name)
+    
+    
+class MayaMainWidget(QWidget):
+    def __init__(self, parent=None, title_name="DefaultTitle", object_name=None, *args, **kwargs):
+        super(MayaMainWidget, self).__init__(parent=None, *args, **kwargs)
+        if parent is None:
+            setWidgetAsMayaMainWindow(self, title_name, object_name)
+        
+        self.main = parent
