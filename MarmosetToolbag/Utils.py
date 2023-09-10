4dc0c305a50308e5b129325db268fb90e23d7d9b
Armageddon-15 <hjc014@126.com>
2023-09-11 01:10:58 +0800

    Update Utils.py

--- MarmosetToolbag/Utils.py
+++ MarmosetToolbag/Utils.py
@@ -4,3 +4,13 @@ import os
 def makeDir(path):
     if not os.path.exists(path):
         os.makedirs(path)
+
+
+def getCertainTypeWithCertainFunc(func, t):
+    all_objects = func()
+    certain_objects = []
+    for obj in all_objects:
+        if type(obj) is t:
+            certain_objects.append(obj)
+
+    return certain_objects
